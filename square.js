1ba426017344a3b9aecec91e952a44ea4c85b7f7
George Carder <georgercarder@gmail.com>
2018-02-15 08:53:13 -0800

    TouchableHighlight to board

--- square.js
+++ square.js
@@ -1,48 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { TouchableHighlight, StyleSheet, Text, View, Image} from 'react-native';
 
 function Square(props) {
   if(props.rank===2){
     return (
-      <Text style={styles.square2} onPress={() => props.onPress()} >
-        {props.value}
-      </Text>
+      <TouchableHighlight style={styles.square2} onPress={() => props.onPress()} >
+        <Text style={styles.txtb1}>{props.value}</Text>
+      </TouchableHighlight>
     );
   } else {
     return (
-      <Text style={styles.square3} onPress={() => props.onPress()} >
-        {props.value}
-      </Text>
+      <TouchableHighlight style={styles.square3} onPress={() => props.onPress()} >
+        <Text style={styles.txtb2}>{props.value}</Text>
+      </TouchableHighlight>
     );  
   }
 }
 
 const styles = StyleSheet.create({
   square2: {
-    backgroundColor: 'white',
-    color: 'black',
-    fontSize: 28,
+		backgroundColor: 'white',
     borderWidth: 1,
     borderColor: '#FEFDE3',
     margin: 1,
     padding: 8,
-    textAlign: 'center',
+		alignItems: 'center',
     height: 60,
     width: 60,  
   },  
   
   square3: {
-    backgroundColor: 'white',
-    color: 'black',
-    fontSize: 14,
+		backgroundColor: 'white',
     borderWidth: 1,
     borderColor: '#FEFDE3',
     margin: 1,
     padding: 8,
-    textAlign: 'center',
+		alignItems: 'center',
     height: 35,
     width: 35,
   },
+
+	txtb1: {
+		fontSize: 28,
+		color: 'black',
+	},
+
+	txtb2: {
+		fontSize: 14,
+		color: 'black',
+	},
 
 });
 
